09c3ca9c0aa5ea689a5c1bef5d0869db832fdad5
liuc8023 <13764402356@163.com>
2018-01-12 15:46:05 +0800

    多余代码删除，并加入swagger注释

--- src/main/java/com/springboot/ybt/system/controller/IconController.java
+++ src/main/java/com/springboot/ybt/system/controller/IconController.java
@@ -7,26 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 @RequestMapping("/icon")
+@Api(value = "图标controller", tags = { "图标操作接口" })
 public class IconController {
 	private static Logger logger = LoggerFactory.getLogger(IconController.class);
 
+	@ApiOperation(value = "跳转到图标选择页面", notes = "跳转到图标选择页面",hidden = true)
 	@RequestMapping(value="turnToIconList",method=RequestMethod.GET)
 	public ModelAndView  turnToIconList(HttpServletRequest request) {
-		 logger.info("跳转到图表选择页面");
+		 logger.info("跳转到图标选择页面");
 		 String iconName = request.getParameter("iconName");
 		 ModelAndView modelAndView = new ModelAndView("system/icon/icon_selector");  
 	     modelAndView.addObject("iconName", iconName);  
 	     return modelAndView;  
 	}
-	
-    @RequestMapping("/nodecorator/select")
-    private String index(String iconName, HttpServletRequest request) {
-
-        request.setAttribute("iconName", iconName);
-        return "base/icon/icon_selector";
-    }
 }
